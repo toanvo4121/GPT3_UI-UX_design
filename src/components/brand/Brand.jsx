b78ca4c13f2a9b0836d51854b8b8c836235c760b
Võ Minh Toàn <toanvo7120@gmail.com>
2022-03-22 18:04:38 +0700

    header container & brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './brand.css'
+import { google, slack, atlassian, dropbox, shopify } from './importIMG'
 
 const Brand = () => {
-  return <div>Brand</div>
+  return (
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt='' />
+      </div>
+      <div>
+        <img src={slack} alt='' />
+      </div>
+      <div>
+        <img src={atlassian} alt='' />
+      </div>
+      <div>
+        <img src={dropbox} alt='' />
+      </div>
+      <div>
+        <img src={shopify} alt='' />
+      </div>
+    </div>
+  )
 }
 
 export default Brand
